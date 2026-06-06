be3b0f3f685beb5d53343030006e1c7def8c36f9
Francisco Cardinali <franciscopcardinali@gmail.com>
2026-06-05 22:14:49 -0300

    Corregir configuracion de pagina duplicada

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import streamlit as st
 from model import ProductoModel
 from controller import SupermercadoController
 
-st.set_page_config(page_title="Carreful", page_icon="🛒")
-
+# --- UNICA configuracion de pagina ---
 st.set_page_config(
-    page_title="Carreful - Panel Familiar",
+    page_title="Carreful", 
     page_icon="🛒",
     layout="centered",
     initial_sidebar_state="collapsed"
